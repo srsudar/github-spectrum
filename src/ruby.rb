1e7bbd252f5e46aef136d7d3078b6dd3d10f8d84
Sam Sudar <sudar.sam@gmail.com>
2015-05-19 22:39:35 -0700

    add ruby 18

---
+++ src/ruby.rb
@@ -0,0 +1,16 @@
+class HelloWorld
+  def sayHello
+    puts "Hello World!"
+  end
+end
+
+# 456789012
+# 4567890
+# 4567890
+# 4567890
+# 4567890
+# 4567890
+# 4567890
+
+hello = HelloWorld.new()
+hello.sayHello
